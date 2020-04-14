8a5f416cef017459ce443459c03896953499b27f
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-04-14 19:47:52 +0200

    Test cases added in  odd numbers spec

--- spec/odd_numbers_spec.rb
+++ spec/odd_numbers_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe Tools::Odd_Numbers do
   end
 
   it 'should find the first odd number in the input array' do
-    odd_numbers = Tools::Odd_Numbers.new([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5, 5])
-    expect(odd_numbers.first_odd_number).to eq 5
+    odd_numbers = Tools::Odd_Numbers.new([1, 1, 2, -2, 8, 2, 4, 4, -1, -2, 8, 8])
+    expect(odd_numbers.first_odd_number).to eq 8
   end
 
   it 'returns the same array if the array has unique elements' do
-    number = Tools::Odd_Numbers.new([5, 6, 7, 1, 2, 3])
-    expect(number.all_odd_numbers).to eq [5, 6, 7, 1, 2, 3]
+    odd_numbers = Tools::Odd_Numbers.new([5, 6, 7, 1, 2, 3])
+    expect(odd_numbers.all_odd_numbers).to eq [5, 6, 7, 1, 2, 3]
   end
 
   it 'return all of the integers which appears an odd number of times' do
-    number = Tools::Odd_Numbers.new([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5, 5])
-    expect(number.all_odd_numbers).to eq [5, -1]
+    odd_numbers = Tools::Odd_Numbers.new([1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5, 5])
+    expect(odd_numbers.all_odd_numbers).to eq [5, -1]
   end
 end
