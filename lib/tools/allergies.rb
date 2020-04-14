f275efea9d85327f3729530a38984261d5a05934
Aleksandar Apostolov <a_aleksandar86@yahoo.com>
2020-04-14 15:20:03 +0200

    Allergies rb file added

--- lib/tools/allergies.rb
+++ lib/tools/allergies.rb
@@ -1,44 +1,48 @@
+module Tools
+
 # class Alergies
-class Allergies
+  class Allergies
 
-  TRESHHOLD = 256
+    TRESHHOLD = 256
 
-  ALLERGENS = {
-    1 => 'eggs',
-    2 => 'peanuts',
-    4 => 'shellfish',
-    8 => 'strawberries',
-    16 => 'tomatoes',
-    32 => 'chocolate',
-    64 => 'pollen',
-    128 => 'cats',
-  }
+    ALLERGENS = {
+      1 => 'eggs',
+      2 => 'peanuts',
+      4 => 'shellfish',
+      8 => 'strawberries',
+      16 => 'tomatoes',
+      32 => 'chocolate',
+      64 => 'pollen',
+      128 => 'cats'
+    }
 
-  def initialize(score)
-    @score = score
-  end
+    def initialize(score)
+      @score = score
+    end
 
-  def allergic_to?(allergen)
-    score_allergens.include?(allergen)
-  end
+    def allergic_to?(allergen)
+      score_allergens.include?(allergen)
+    end
 
-  def score_allergens
-    allergen_keys.map { |key| ALLERGENS[key] }
-  end
+    def score_allergens
+      allergen_keys.map { |key| ALLERGENS[key] }
+    end
 
-  private
+    private
 
-  def total_score
-    @score -= TRESHHOLD while @score >= TRESHHOLD
-    @score
-  end
+    def total_score
+      @score -= TRESHHOLD while @score >= TRESHHOLD
+      @score
+    end
 
-  def keys
-    ALLERGENS.keys.select { |item| item <= total_score }.sort.reverse!
-  end
+    def keys
+      ALLERGENS.keys.select { |item| item <= total_score }.sort.reverse!
+    end
 
-  def allergen_keys
-    tmp = score
-    keys.select { |elem| tmp >= elem && tmp -= elem }
+    def allergen_keys
+      tmp = total_score
+      keys.select { |elem| tmp >= elem && tmp -= elem }
+    end
+
   end
 end
